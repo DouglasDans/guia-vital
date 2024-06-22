fd60182c7098d84f7509e0736f8f6b2e8bca6520
douglasdans <douglas.dans@gmail.com>
2024-06-21 23:31:19 -0300

    teste pwa 3

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+var cacheName = 'Wikiflix';
+
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
