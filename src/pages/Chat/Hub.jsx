851d2e04df3e66157f07962362107e8a93cddd81
douglasdans <douglas.dans@gmail.com>
2024-06-23 19:51:39 -0300

    Adiciona Hub do Chat

--- src/pages/Chat/Hub.jsx
+++ src/pages/Chat/Hub.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import Navbar from '../../components/global/Navbar'
 import TitlePage from '../../components/global/TitlePage'
 import Container from '../../components/Container'
+import { Link } from 'react-router-dom'
 
 export default function Hub() {
    return (
@@ -11,9 +12,21 @@ export default function Hub() {
          <TitlePage title={"Chat"}/>
 
          <Container>
-            a
+            <Link to={'/chat/mensagem'} style={style}>
+               <small>Hora</small>
+               <span style={{fontWeight: '600'}}>8h00</span>
+            </Link>
          </Container>
 
       </Fragment>
    )
+}
+
+const style = {
+   display:'flex', 
+   flexDirection: 'column',
+   backgroundColor: "#f5f5f5",
+   padding: '1rem',
+   textDecoration: 'none',
+   color: 'black'
 }
