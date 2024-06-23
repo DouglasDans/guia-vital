5773c0b64d4bc912d36d905c59d4194ba0cda7b0
douglasdans <douglas.dans@gmail.com>
2024-06-23 20:19:11 -0300

    Corrige erro na Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
         <div style={{border: '1px solid rgba(0, 0, 0, 0.15)', padding: '2rem 1rem', borderRadius: '4px'}}>
           <span style={{fontSize: '1.3rem'}}>Como você está se sentindo</span>
           <p>É importante que você nos diga como está se sentindo para que um médico te ajude, se necessário</p>
-          <Link to={'/'} style={{background: '#C7F5FF', fontWeight: '600', padding: '0.5rem 2rem', textDecoration: 'none', color: 'black', borderRadius: '4px'}}>Registrar Idiomas</Link>
+          <Link to={'/'} style={{background: '#C7F5FF', fontWeight: '600', padding: '0.5rem 2rem', textDecoration: 'none', color: 'black', borderRadius: '4px'}}>Registrar Sintomas</Link>
         </div>
       </Container>
 
