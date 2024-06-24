8ab2db4dd18770db580ba70a7d1ef844d1a5f5cd
douglasdans <douglas.dans@gmail.com>
2024-06-23 21:42:31 -0300

    Corrige tela do Chat

--- src/components/Chat/TitlePageChat.jsx
+++ src/components/Chat/TitlePageChat.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './titlePage.module.css'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Link } from 'react-router-dom';
 export default function TitlePageChat({title}) {
    return (
       <div className={styles.titlePageContainer}>
-         <ArrowBackIcon/>
+         <Link to={"/chat"} style={{color: 'black'}}><ArrowBackIcon/></Link>
          <h1 className={styles.title}>{title}</h1>
          <p></p>
       </div>
