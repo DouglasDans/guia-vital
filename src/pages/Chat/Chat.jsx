6f7b56823dbca709e3d5e178c29b6e80fe7942e5
douglasdans <douglas.dans@gmail.com>
2024-06-23 21:44:31 -0300

    Corrige Tela do Chat

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -8,12 +8,12 @@ export default function Chat() {
       <TitlePageChat title={'Doutor Preston Burke'}/>
       <Container>
         <div>
-          <div style={{position: 'absolute', bottom: '80px', width: '100vw', display: 'flex', justifyContent: 'flex-end'}}>
+          <div style={{position: 'absolute', bottom: '80px', width: '95vw', display: 'flex', justifyContent: 'flex-end'}}>
             <span style={{background: '#F3F3F3', padding: '1rem', borderRadius: '4px'}}>
               Olá Doutor
             </span>
           </div>
-          <div style={{position: 'absolute', bottom: '0px', width: '99%', lineHeight: '30px', display: 'flex', justifyContent: 'space-between', padding: '1rem', gap: '1rem'}}>
+          <div style={{position: 'absolute', bottom: '0px', width: '95%', lineHeight: '30px', display: 'flex', justifyContent: 'space-between', padding: '1rem', gap: '1rem'}}>
             <input type="text" placeholder={'Digite sua Mensagem'}
               style={{height: '30px', width: "100%", border: '1px solid #ADADAD'}}
             />
